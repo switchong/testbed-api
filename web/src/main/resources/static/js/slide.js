a119eebade1d275b4fd8a1ca7b300bd73d960adb
easyhoon119 <easyhoon119@naver.com>
2022-01-19 09:22:50 +0900

    갤러리 화면 퍼블 및 갤러리 구조 변경

--- web/src/main/resources/static/js/slide.js
+++ web/src/main/resources/static/js/slide.js
@@ -1,56 +1,82 @@
-window.addEventListener('DOMContentLoaded',() => {
-    const prevBtn = document.querySelector('#prevBtn');
-    const nextBtn = document.querySelector('#nextBtn');
-    const slideContainer = document.querySelector('.gallery-slide-list-container');
-    const slides = document.querySelectorAll('.gallery-slide-list');
-    let currentNum = 0;
+let currentNum = 0;
+let prevBtn;
+let nextBtn;
+let slideContainer;
+let slides;
+
+const goNext = () => {
+    if(slides.length !== currentNum + 1) {
+        currentNum++;
+        slides.forEach((item)=>{
+            item.style.transform = 'scale(1)';
+        });
+        console.log(currentNum, slides.length);
+        slideContainer.style.transform = `translateX(${-(slides[currentNum].getBoundingClientRect().left - slideContainer.getBoundingClientRect().left)}px)`;
+        slides.forEach((item, index)=>{
+            if(index !== currentNum) {
+                item.style.transform = 'scale(0.9)';
+            }
+        });
+    }
+}
+
+const goPrev = () => {
+    if(currentNum > 0) {
+        currentNum--;
+        slides.forEach((item)=>{
+            item.style.transform = 'scale(1)';
+        });
+        console.log(currentNum, slides.length);
+        slideContainer.style.transform = `translateX(${-(slides[currentNum].getBoundingClientRect().left - slideContainer.getBoundingClientRect().left)}px)`;
+        slides.forEach((item, index)=>{
+            if(index !== currentNum) {
+                item.style.transform = 'scale(0.9)';
+            }
+        });
+    }
+}
+
+const goFirst = () => {
+    slides.forEach((item)=>{
+        item.style.transform = 'scale(1)';
+    });
+    console.log(currentNum, slides.length);
+    slideContainer.style.transform = 'translateX(0px)';
+    currentNum = 0;
+    slides.forEach((item, index)=>{
+        if(index !== currentNum) {
+            item.style.transform = 'scale(0.9)';
+        }
+    });
+}
+
+const Refresh = () => {
+    window.removeEventListener('resize', goFirst);
+    prevBtn.removeEventListener('click',goPrev);
+    nextBtn.removeEventListener('click',goNext);
+    goSlide();
+}
+
+const goSlide = () => {
+    prevBtn = document.querySelector('#prevBtn');
+    nextBtn = document.querySelector('#nextBtn');
+    slideContainer = document.querySelector('.gallery-slide-list-container');
+    slides = document.querySelectorAll('.gallery-slide-list');
+
     slides.forEach((item, index)=>{
         if(index !== currentNum) {
             item.style.transform = 'scale(0.9)';
         }
     });
 
-    window.addEventListener('resize',()=>{
-        slides.forEach((item)=>{
-            item.style.transform = 'scale(1)';
-        });
-        slideContainer.style.transform = 'translateX(0px)';
-        currentNum = 0;
-        slides.forEach((item, index)=>{
-            if(index !== currentNum) {
-                item.style.transform = 'scale(0.9)';
-            }
-        });
-    })
+    console.log(currentNum, slides.length);
 
-    prevBtn.addEventListener('click',()=>{
 
-         if(currentNum > 0) {
-            currentNum--;
-             slides.forEach((item)=>{
-                     item.style.transform = 'scale(1)';
-             });
-            slideContainer.style.transform = `translateX(${-(slides[currentNum].getBoundingClientRect().left - slideContainer.getBoundingClientRect().left)}px)`;
-            slides.forEach((item, index)=>{
-                if(index !== currentNum) {
-                    item.style.transform = 'scale(0.9)';
-                }
-            });
-        }
-    })
+    window.addEventListener('resize',goFirst);
 
-    nextBtn.addEventListener('click',()=>{
-        if(slides.length !== currentNum + 1) {
-            currentNum++;
-            slides.forEach((item)=>{
-                item.style.transform = 'scale(1)';
-            });
-            slideContainer.style.transform = `translateX(${-(slides[currentNum].getBoundingClientRect().left - slideContainer.getBoundingClientRect().left)}px)`;
-            slides.forEach((item, index)=>{
-                if(index !== currentNum) {
-                    item.style.transform = 'scale(0.9)';
-                }
-            });
-        }
-    })
-})
+    prevBtn.addEventListener('click',goPrev);
+
+    nextBtn.addEventListener('click',goNext);
+}
+
+window.addEventListener('DOMContentLoaded',goSlide);
